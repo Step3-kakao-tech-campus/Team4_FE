fb85b287d0aefcc92df91f16c6e0a223aa616ce5
gomdarachi <=>
2023-10-03 19:02:57 +0900

    feat: 마이 페이지 다국어 번역 적용 및 tsx로 변환

--- src/components/molecules/myPageList.tsx
+++ src/components/molecules/myPageList.tsx
@@ -1,7 +1,12 @@
 import { AiOutlineRight } from 'react-icons/ai';
 import { Link } from 'react-router-dom';
 
-function myPageList({ text, linkTo }) {
+interface IMyPageListProps {
+  text: string;
+  linkTo: string;
+}
+
+function MyPageList({ text, linkTo } : IMyPageListProps) {
   return (
     <Link to={linkTo}>
       <div className="bg-white flex justify-between my-9 text-base">
@@ -12,4 +17,4 @@ function myPageList({ text, linkTo }) {
   );
 }
 
-export default myPageList;
+export default MyPageList;
