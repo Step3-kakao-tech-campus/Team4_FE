e5f4f7a4875888c013da86b8f78bb2897ba51cea
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-07 17:40:18 +0900

    feat: 버튼 추가 스타일링을 위한 prop 추가

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -4,6 +4,7 @@ interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
   size?: 'small' | 'medium';
   backgroundColor?: string;
   textColor?: string;
+  extraStyle?: string;
   children: React.ReactNode;
 }
 
@@ -11,15 +12,16 @@ export default function Button({
   size = 'medium',
   backgroundColor = 'bg-matgpt-blue',
   textColor = 'text-white',
+  extraStyle = '',
   children,
   ...props
 }: ButtonProps) {
   return (
     <button
       type="button"
-      className={`${size === 'medium' ? 'px-8 py-2 text-base' : 'px-4 py-1 text-xs'} 
-        ${backgroundColor} ${textColor} break-keep rounded-full`}
       {...props}
+      className={`${size === 'medium' ? 'px-8 py-2 text-base' : 'px-4 py-1 text-xs'} 
+        ${backgroundColor} ${textColor} break-keep rounded-full ${extraStyle}`}
     >
       {children}
     </button>
