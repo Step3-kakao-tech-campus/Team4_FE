896d6972eaff74b744314115931c19ce24dd17ec
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-07 17:20:53 +0900

    feat: 버튼 컴포넌트 구현

--- src/components/atoms/button.tsx
+++ src/components/atoms/button.tsx
@@ -1,14 +1,24 @@
 import { ButtonHTMLAttributes } from 'react';
 
 interface ButtonProps extends ButtonHTMLAttributes<HTMLButtonElement> {
+  size?: 'small' | 'medium';
+  backgroundColor?: string;
+  textColor?: string;
   children: React.ReactNode;
 }
 
-export default function Button({ children, ...props }: ButtonProps) {
+export default function Button({
+  size = 'medium',
+  backgroundColor = 'bg-matgpt-blue',
+  textColor = 'text-white',
+  children,
+  ...props
+}: ButtonProps) {
   return (
     <button
       type="button"
-      className="rounded-full bg-matgpt-blue px-8 py-2 text-white"
+      className={`${size === 'medium' ? 'px-8 py-2 text-base' : 'px-4 py-1 text-xs'} 
+        ${backgroundColor} ${textColor} break-keep rounded-full`}
       {...props}
     >
       {children}
