c368110e6994eebb77ceeab6f6b74fbad5b38413
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-08 17:56:43 +0900

    refactor: 중복 코드 분리

--- src/components/molecules/menuTag.tsx
+++ src/components/molecules/menuTag.tsx
@@ -3,6 +3,26 @@ import { RefHandler } from '../../types/refHandler';
 import Input from '../atoms/input';
 import Button from '../atoms/button';
 
+interface DefaultTagProps {
+  name: string;
+  onClick: React.MouseEventHandler<HTMLButtonElement>;
+}
+
+function DefaultTag({ name, onClick }: DefaultTagProps) {
+  return (
+    <div className="relative flex w-[10rem] flex-col items-center">
+      <button
+        type="button"
+        className="flex rounded-lg bg-black px-3 py-2"
+        onClick={onClick}
+      >
+        <span className="text-sm text-white">{name}</span>
+      </button>
+      <div className="w-4 border-[0.5rem] border-transparent border-t-black" />
+    </div>
+  );
+}
+
 interface MenuTagProps {
   name: string;
   mode: 'modify' | 'prompt';
@@ -49,16 +69,7 @@ const MenuTag = forwardRef<RefHandler, MenuTagProps>((
             <div className="w-4 border-[0.5rem] border-transparent border-t-black" />
           </div>
         ) : (
-          <div className="relative flex w-[10rem] flex-col items-center">
-            <button
-              type="button"
-              className="rounded-xl bg-black px-4 py-2"
-              onClick={() => setIsExpanded(true)}
-            >
-              <span className="text-white">{name}</span>
-            </button>
-            <div className="w-4 border-[0.5rem] border-transparent border-t-black" />
-          </div>
+          <DefaultTag name={name} onClick={() => setIsExpanded(true)} />
         )}
       </div>
     );
@@ -69,10 +80,10 @@ const MenuTag = forwardRef<RefHandler, MenuTagProps>((
       <div>
         {isExpanded ? (
           <div className="relative flex w-[10rem] flex-col items-center">
-            <section className="flex flex-col rounded-xl bg-black px-4 py-2 text-white">
-              <h3>{name}</h3>
-              <p>프롬프트에 추가하시겠습니까?</p>
-              <div className="mt-1 flex flex-row justify-around">
+            <section className="flex flex-col gap-1 rounded-xl bg-black px-4 py-2 text-white">
+              <h3 className="font-bold">{name}</h3>
+              <p className="text-xs">프롬프트에 추가하시겠습니까?</p>
+              <div className="flex flex-row justify-around">
                 <Button
                   size="small"
                   textColor="text-black"
@@ -93,16 +104,7 @@ const MenuTag = forwardRef<RefHandler, MenuTagProps>((
             <div className="w-4 border-[0.5rem] border-transparent border-t-black" />
           </div>
         ) : (
-          <div className="relative flex w-[10rem] flex-col items-center">
-            <button
-              type="button"
-              className="rounded-xl bg-black px-4 py-2"
-              onClick={() => setIsExpanded(true)}
-            >
-              <span className="text-white">{name}</span>
-            </button>
-            <div className="w-4 border-[0.5rem] border-transparent border-t-black" />
-          </div>
+          <DefaultTag name={name} onClick={() => setIsExpanded(true)} />
         ) }
       </div>
     );
