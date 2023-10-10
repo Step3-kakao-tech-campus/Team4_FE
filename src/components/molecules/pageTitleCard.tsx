facb9cc821f93daa6d36118003a35089679df8e3
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-10 17:32:11 +0900

    feat: 페이지 제목 카드 다국어 처리

--- src/components/molecules/pageTitleCard.tsx
+++ src/components/molecules/pageTitleCard.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import Icon from '../atoms/icon';
 
 interface PageTitleCardProps {
@@ -5,10 +6,11 @@ interface PageTitleCardProps {
 }
 
 export default function PageTitleCard({ pageTitle }: PageTitleCardProps) {
+  const { t } = useTranslation();
   return (
     <div className="flex w-full items-center gap-4 bg-matgpt-blue p-4 text-white">
       <button type="button">
-        <Icon name="OutlineLeft" size="1.2rem" ariaLabel="뒤로 가기" />
+        <Icon name="OutlineLeft" size="1.2rem" ariaLabel={t('pageTitle.back')} />
       </button>
       <h1 className="flex grow items-center text-lg font-bold">
         {pageTitle}
