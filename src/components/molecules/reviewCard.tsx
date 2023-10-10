82a875769419308ed39fb5c95c9bb3da6332783a
gomdarachi <=>
2023-10-10 19:17:12 +0900

    feat: 리뷰카드 컴포넌트 구현 완료
    - public/image/fakeDb/reviewCard에 가게 대표이미지 / 유저 프로필 이미지
      추가 했습니다.
    - css넣는 과정에서 닉네임이나 가게이름의 길이가 다음 줄로 넘어가면
      어떻게 처리 할지 모르겠어서, 우선 그냥 다음줄로 넘어가게
      뒀습니다(리뷰로 개선방안 알려주시면 적용하겠습니다)

--- src/components/molecules/reviewCard.tsx
+++ src/components/molecules/reviewCard.tsx
@@ -1,3 +1,4 @@
+import { Link } from 'react-router-dom';
 import Icon from '../atoms/icon';
 import Image from '../atoms/image';
 
@@ -5,39 +6,52 @@ interface ReviewCardType {
   storeImage: string;
   storeName: string;
   profileImage: string;
-  reviewer: string;
-  reviewRating: string;
-  memberNumber: string;
-  whenIsDate: string;
+  reviewerName: string;
+  reviewRating: number;
+  memberNumber: number;
+  createdAt: string;
 }
 
 function ReviewCard({
-  storeImage, storeName, profileImage, reviewer, reviewRating, memberNumber, whenIsDate,
+  storeImage, storeName, profileImage, reviewerName, reviewRating, memberNumber, createdAt,
 }:ReviewCardType) {
+  const storeId = 1;
+  const reviewId = 2;
   return (
-    <div className="flex">
-      <div className="mr-2">
-        <Image imageSrc={storeImage} alt="맛집 이미지" threshold={{ smallToMedium: '0', mediumToLarge: '0' }} />
-      </div>
-      <div className="w-[20rem] py-1">
-        <div>
-          <span className="font-bold">{storeName}</span>
+    <Link
+      to={`/stores/${storeId}/reviews/${reviewId}`}
+      className="flex w-full items-center gap-4 p-2"
+      aria-label={`${storeName} 상세 페이지로 이동`}
+    >
+      <div className="flex">
+        <div className="mr-2 h-34 w-26">
+          <Image imageSrc={storeImage} alt="맛집 이미지" threshold={{ smallToMedium: '0', mediumToLarge: '0' }} />
         </div>
-        <div className="flex items-center py-1">
-          <Image imageSrc={profileImage} alt="유저이미지" threshold={{ smallToMedium: '0', mediumToLarge: '0' }} className="w-[2.5rem] pr-2" />
-          <span>{reviewer}</span>
-        </div>
-        <div className="flex justify-between">
-          <div className="flex items-center">
-            <Icon name="OutlineStar" size="1.5rem" ariaLabel="평점 점수" />
-            <span className="pl-1 pr-3">{reviewRating}</span>
-            <Icon name="UserGroup" size="1.2rem" ariaLabel="인원 수" />
-            <span className="pl-1">{memberNumber}</span>
+        <div className="w-[20rem]">
+          <div>
+            <span className="font-bold h-[0.1rem] overflow-hidden">{storeName}</span>
           </div>
-          <span>{whenIsDate}</span>
+          <div className="flex items-center py-2">
+            <div className="w-[2.5rem] h-[31.901px] ">
+              <Image imageSrc={profileImage} alt="유저이미지" threshold={{ smallToMedium: '0', mediumToLarge: '0' }} className="pr-2" />
+            </div>
+            <span className="h-[1.5rem] overflow-hidden">{reviewerName}</span>
+          </div>
+          <div className="flex justify-between">
+            <div className="flex items-center">
+              <Icon name="OutlineStar" size="1.5rem" ariaLabel="평점 점수" />
+              <span className="pl-1 pr-3">{reviewRating}</span>
+              <Icon name="UserGroup" size="1.2rem" ariaLabel="인원 수" />
+              <span className="pl-1">
+                {memberNumber}
+                명
+              </span>
+            </div>
+            <span>{createdAt}</span>
+          </div>
         </div>
       </div>
-    </div>
+    </Link>
   );
 }
 
