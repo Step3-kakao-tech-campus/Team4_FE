03f2db3f7efb32a4511a929a61dd11b6bf405dcb
gomdarachi <=>
2023-10-10 19:30:18 +0900

    config: App.tsx 테스트 Route 삭제

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,6 @@
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import LandingPage from './components/page/landingPage';
 import UserPage from './components/page/userPage';
-import ReviewCard from './components/molecules/reviewCard';
 
 function App() {
   return (
@@ -10,20 +9,6 @@ function App() {
         <Route>
           <Route path="/landing" element={<LandingPage />} />
           <Route path="/mypage" element={<UserPage />} />
-          <Route
-            path="/test"
-            element={(
-              <ReviewCard
-                storeImage="/image/fakeDb/reviewCard/storeImage.png"
-                storeName="몽준헌 판교점"
-                profileImage="/image/fakeDb/userPage/userImage.png"
-                reviewerName="작성자"
-                reviewRating={4.5}
-                memberNumber={2}
-                createdAt="7개월 전"
-              />
-            )}
-          />
         </Route>
       </Routes>
     </BrowserRouter>
