a0d746c9702873d6ba7a36373819f3115e13475e
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-12 19:59:19 +0900

    feat: 음식점 마커 추가
    - 음식점 이름 및 음식점 대표 이미지 표시
    - 클릭 시 음식점 상세 페이지로 이동

---
+++ src/components/molecules/marker.tsx
@@ -0,0 +1,29 @@
+import { OverlayViewF } from '@react-google-maps/api';
+import { Link } from 'react-router-dom';
+import Image from '../atoms/image';
+import { MarkerInfo } from '../../types/map';
+
+export default function Marker({
+  lat,
+  lng,
+  storeId,
+  storeName,
+  image,
+}: MarkerInfo) {
+  return (
+    <OverlayViewF
+      mapPaneName="floatPane"
+      position={{ lat, lng }}
+    >
+      <Link
+        className="absolute -left-6 -top-6 flex flex-col items-center justify-center gap-2"
+        to={`/stores/${storeId}`}
+      >
+        <h3 className="absolute -top-7 w-28 truncate break-all rounded-full border border-black bg-white px-2 py-1 text-center text-[0.8125rem] font-bold">{storeName}</h3>
+        <div className="h-12 w-12 rounded-full border-2 border-black bg-matgpt-blue object-cover p-1">
+          <Image imageSrc={image} alt={`${storeName} 대표 이미지`} objectFitMode />
+        </div>
+      </Link>
+    </OverlayViewF>
+  );
+}
