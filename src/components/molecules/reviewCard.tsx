9e5dc25fc994baa3813b30230101ce58bb963331
gomdarachi <=>
2023-10-12 20:32:56 +0900

    config: pr 코멘트에 맞춰 수정 방문한 인원 -> visitedCount로 수정 text-overflow: ellipsis 속성 추가 storeId, reviewId Props 추가 오타 수정 아이콘 단어 제외

--- src/components/molecules/reviewCard.tsx
+++ src/components/molecules/reviewCard.tsx
@@ -4,21 +4,21 @@ import Icon from '../atoms/icon';
 import Image from '../atoms/image';
 
 interface ReviewCardType {
+  storeId: number,
+  reviewId: number,
   storeImage: string;
   storeName: string;
   profileImage: string;
   reviewerName: string;
   reviewRating: number;
-  memberNumber: number;
+  visitedCount: number;
   createdAt: string;
 }
 
 function ReviewCard({
-  storeImage, storeName, profileImage, reviewerName, reviewRating, memberNumber, createdAt,
+  storeId, reviewId, storeImage, storeName, profileImage, reviewerName, reviewRating, visitedCount, createdAt,
 }:ReviewCardType) {
   const { t } = useTranslation();
-  const storeId = 1;
-  const reviewId = 2;
   return (
     <Link
       to={`/stores/${storeId}/reviews/${reviewId}`}
@@ -31,13 +31,13 @@ function ReviewCard({
         </div>
         <div className="w-[20rem]">
           <div>
-            <span className="font-bold h-[0.1rem] overflow-hidden">{storeName}</span>
+            <span className="font-bold h-[0.1rem] overflow-hidden text-ellipsis">{storeName}</span>
           </div>
           <div className="flex items-center py-2">
             <div className="w-[2.5rem] h-[31.901px] ">
               <Image imageSrc={profileImage} alt={t('reviewCard.userImage')} threshold={{ smallToMedium: '0', mediumToLarge: '0' }} className="pr-2" />
             </div>
-            <span className="h-[1.5rem] overflow-hidden">{reviewerName}</span>
+            <span className="h-[1.5rem] overflow-hidden text-ellipsis">{reviewerName}</span>
           </div>
           <div className="flex justify-between">
             <div className="flex items-center">
@@ -45,7 +45,7 @@ function ReviewCard({
               <span className="pl-1 pr-3">{reviewRating}</span>
               <Icon name="UserGroup" size="1.2rem" ariaLabel={t('reviewCard.memberNumIcon')} />
               <span className="pl-1">
-                {memberNumber}
+                {visitedCount}
                 명
               </span>
             </div>
