3bf8f6d8d761e49f2bc9c54041ea27449366fb45
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-13 20:03:50 +0900

    fix: axios 및 react query 에러 해결
    - localhost 프로토콜 문제 해결
    - 브라우저 창 focus 될 때 refetch 하는 문제 해결

--- src/apis/instance.ts
+++ src/apis/instance.ts
@@ -1,3 +1,3 @@
 import { createFetchInstance } from '../utils/fetch';
 
-export const fetchInstance = createFetchInstance('localhost:3000', 1000);
+export const fetchInstance = createFetchInstance('http://localhost:3000', 1000);
