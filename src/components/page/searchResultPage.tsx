2c9bd6631739f94aeaee9972281489585a276a97
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-14 19:55:56 +0900

    refactor: intersectionObserver 커스텀 훅 구현

--- src/components/page/searchResultPage.tsx
+++ src/components/page/searchResultPage.tsx
@@ -1,36 +1,21 @@
 import { useSearchParams } from 'react-router-dom';
-import { useRef, useEffect, useState } from 'react';
 import SearchResultTemplate from '../template/searchResultTemplate';
 import { useSearchStore } from '../../hooks/query';
+import { useIntersectionObserver } from '../../hooks/intersectionObserver';
 
 export default function SearchResultPage() {
   const [searchParams] = useSearchParams();
   const searchString = searchParams.get('q');
-  const [pageIndex, setPageIndex] = useState(0);
 
   const {
     data, isFetching, isLoading, hasNextPage, fetchNextPage,
   } = useSearchStore(searchString);
 
-  const bottomObserverRef = useRef<HTMLDivElement>(null);
-
-  const io = new IntersectionObserver((entries) => {
-    entries.forEach((entry) => {
-      if (entry.isIntersecting) {
-        if (hasNextPage !== false && bottomObserverRef.current !== null) {
-          io.unobserve(bottomObserverRef.current);
-          setPageIndex((prev) => prev + 1);
-        }
-      }
-    });
-  }, { threshold: 1 });
-
-  useEffect(() => {
-    if (hasNextPage !== false && bottomObserverRef.current !== null) {
+  const bottomObserverRef = useIntersectionObserver(() => {
+    if (hasNextPage) {
       fetchNextPage();
-      io.observe(bottomObserverRef.current);
     }
-  }, [pageIndex]);
+  }, { threshold: 1 });
 
   return (
     <>
