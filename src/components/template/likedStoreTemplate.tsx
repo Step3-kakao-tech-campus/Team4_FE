571ece87ef173ca879b691402e7c582b39aff454
gomdarachi <=>
2023-10-15 21:19:46 +0900

    refactor: left, right onClick 함수화

--- src/components/template/likedStoreTemplate.tsx
+++ src/components/template/likedStoreTemplate.tsx
@@ -10,6 +10,14 @@ interface LikedStoreTemplateType {
 }
 
 function LikedStoreTemplate({ likedStore, page, onChangePage }: LikedStoreTemplateType) {
+  function rightPage() {
+    if (likedStore.length < 6) alert('마지막 페이지 입니다.'); else onChangePage('right');
+  }
+  function leftPage() {
+    if (page > 1) onChangePage('left');
+    else alert('첫 번째 페이지 입니다.!');
+  }
+
   return (
     <div className="relative">
       <div className="sticky top-0">
@@ -17,10 +25,7 @@ function LikedStoreTemplate({ likedStore, page, onChangePage }: LikedStoreTempla
       </div>
       <nav className="flex items-center justify-between px-4 py-2">
         <button
-          onClick={() => {
-            if (page > 1) onChangePage('left');
-            else alert('첫 번째 페이지 입니다.!');
-          }}
+          onClick={() => { leftPage(); }}
           type="button"
           className="flex items-center"
         >
@@ -28,7 +33,7 @@ function LikedStoreTemplate({ likedStore, page, onChangePage }: LikedStoreTempla
           <span>이전 페이지로</span>
         </button>
         <div className="h-7 w-7 rounded-lg border border-solid border-indigo-500/40 text-center text-lime-800">{page}</div>
-        <button onClick={() => { if (likedStore.length < 6) alert('마지막 페이지 입니다.'); else onChangePage('right'); }} type="button" className="flex items-center">
+        <button onClick={() => { rightPage(); }} type="button" className="flex items-center">
           <span>다음 페이지로</span>
           <Icon name="OutlineRight" size="2rem" ariaLabel="오른쪽 페이지로 이동" />
         </button>
