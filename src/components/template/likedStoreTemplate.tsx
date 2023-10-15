eb835c15211ed34500f50ab2352fc23706c49d79
gomdarachi <=>
2023-10-15 21:36:25 +0900

    feat: 다국어 구현

--- src/components/template/likedStoreTemplate.tsx
+++ src/components/template/likedStoreTemplate.tsx
@@ -1,3 +1,4 @@
+import { useTranslation } from 'react-i18next';
 import { StoreCardInfo } from '../../types/store';
 import PageTitleCard from '../molecules/pageTitleCard';
 import Icon from '../atoms/icon';
@@ -10,18 +11,19 @@ interface LikedStoreTemplateType {
 }
 
 function LikedStoreTemplate({ likedStore, page, onChangePage }: LikedStoreTemplateType) {
+  const { t } = useTranslation();
   function rightPage() {
-    if (likedStore.length < 6) alert('마지막 페이지 입니다.'); else onChangePage('right');
+    if (likedStore.length < 6) alert(t('likedStorePage.lastPage')); else onChangePage('right');
   }
   function leftPage() {
     if (page > 1) onChangePage('left');
-    else alert('첫 번째 페이지 입니다.!');
+    else alert(t('likedStorePage.firstPage'));
   }
 
   return (
     <div className="relative">
       <div className="sticky top-0">
-        <PageTitleCard pageTitle="좋아요 한 맛집" />
+        <PageTitleCard pageTitle={t('likedStorePage.pageTitle')} />
       </div>
       <nav className="flex items-center justify-between px-4 py-2">
         <button
@@ -29,13 +31,13 @@ function LikedStoreTemplate({ likedStore, page, onChangePage }: LikedStoreTempla
           type="button"
           className="flex items-center"
         >
-          <Icon name="OutlineLeft" size="2rem" ariaLabel="왼쪽 페이지로 이동" />
-          <span>이전 페이지로</span>
+          <Icon name="OutlineLeft" size="2rem" ariaLabel={t('likedStorePage.leftPageIcon')} />
+          <span>{t('likedStorePage.previousPage')}</span>
         </button>
         <div className="h-7 w-7 rounded-lg border border-solid border-indigo-500/40 text-center text-lime-800">{page}</div>
         <button onClick={() => { rightPage(); }} type="button" className="flex items-center">
-          <span>다음 페이지로</span>
-          <Icon name="OutlineRight" size="2rem" ariaLabel="오른쪽 페이지로 이동" />
+          <span>{t('likedStorePage.nextPage')}</span>
+          <Icon name="OutlineRight" size="2rem" ariaLabel={t('likedStorePage.rightPagIcon')} />
         </button>
       </nav>
       <main>
