2795247eaf198f2c6e52ee9d5a394b0d646a9e57
gomdarachi <=>
2023-10-17 00:42:08 +0900

    feat: 코인 충전 내역 타입 구현

--- src/components/template/coinRechargeTemplate.tsx
+++ src/components/template/coinRechargeTemplate.tsx
@@ -1,8 +1,13 @@
 import React from 'react';
+import PageTitleCard from '../molecules/pageTitleCard';
 
 function CoinRechargeTemplate() {
   return (
-    <div>coinRechargeTemplatC</div>
+    <main className="relative">
+      <div className="sticky top-0">
+        <PageTitleCard pageTitle="코인 충전 내역" />
+      </div>
+    </main>
   );
 }
 
