dc22c4212e95c97caeda786ed9d3493b0d65f88c
gomdarachi <=>
2023-10-17 14:26:22 +0900

    config: 페이지 컴포넌트 다국어 작업 - 코인 충전 내역 페이지, 좋아요한 맛집 페이지의 필요없는 다국어 문장 삭제

--- src/components/molecules/page.tsx
+++ src/components/molecules/page.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useTranslation } from 'react-i18next';
 import Icon from '../atoms/icon';
 
 interface PageInterface {
@@ -8,12 +9,13 @@ interface PageInterface {
 }
 
 function Page({ page, isLastPage, onChangePage }: PageInterface) {
+  const { t } = useTranslation();
   function rightPage() {
-    if (isLastPage) alert('마지막 페이지 입니다.'); else onChangePage('right');
+    if (isLastPage) alert(t('page.lastPage')); else onChangePage('right');
   }
   function leftPage() {
     if (page > 1) onChangePage('left');
-    else alert('첫번째 페이지 입니다.');
+    else alert(t('page.firstPage'));
   }
   return (
     <div className="flex items-center justify-between px-4 py-2">
@@ -22,13 +24,13 @@ function Page({ page, isLastPage, onChangePage }: PageInterface) {
         type="button"
         className="flex items-center"
       >
-        <Icon name="OutlineLeft" size="2rem" ariaLabel="이전 페이지로 이동" />
-        <span>이전 페이지로</span>
+        <Icon name="OutlineLeft" size="2rem" ariaLabel={t('page.previousPage')} />
+        <span>{t('page.previousPage')}</span>
       </button>
       <div className="h-7 w-7 rounded-lg border border-solid border-indigo-500/40 text-center text-lime-800">{page}</div>
       <button onClick={() => { rightPage(); }} type="button" className="flex items-center">
-        <span>다음 페이지로</span>
-        <Icon name="OutlineRight" size="2rem" ariaLabel="다음 페이지로 이동" />
+        <span>{t('page.nextPage')}</span>
+        <Icon name="OutlineRight" size="2rem" ariaLabel={t('page.nextPage')} />
       </button>
     </div>
   );
