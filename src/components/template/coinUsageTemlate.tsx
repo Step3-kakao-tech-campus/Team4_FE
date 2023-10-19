8dfede403c4976230858b667debb858966e192db
gomdarachi <=>
2023-10-19 17:21:44 +0900

    feat: CoinUsageInfo 타입 추가

---
+++ src/components/template/coinUsageTemlate.tsx
@@ -0,0 +1,14 @@
+import { useState, useEffect } from 'react';
+import { useTranslation } from 'react-i18next';
+import PageTitleCard from '../molecules/pageTitleCard';
+import { CoinUsageInfo } from '../../types/coin';
+import CoinListCard from '../molecules/coinListCard';
+import Page from '../molecules/page';
+
+function CoinUsageTemlate() {
+  return (
+    <div>coinUsageTemlate</div>
+  );
+}
+
+export default CoinUsageTemlate;
