1aca14d6e83af1424a6bd1301dabac9f35692eb3
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-21 02:49:59 +0900

    feat: 랜딩 페이지 언어 선택 모달 적용

--- src/components/modals/languageModal.tsx
+++ src/components/modals/languageModal.tsx
@@ -20,7 +20,7 @@ export default function LanguageModal() {
 
   return (
     <>
-      <div className="fixed bottom-0 w-full max-w-[500px] rounded-t-2xl ">
+      <div className="fixed bottom-0 w-full max-w-[500px] rounded-t-2xl bg-white">
         <form onSubmit={onLanguageSelect}>
           <section>
             <h1 className="py-2 text-center">{t('landingPage.languageSelect')}</h1>
