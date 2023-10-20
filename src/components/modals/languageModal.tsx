eabce3012d88297172a67f77aa5e641e19261ca7
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-20 20:57:59 +0900

    feat: 언어 선택 모달 추가

---
+++ src/components/modals/languageModal.tsx
@@ -0,0 +1,52 @@
+import { useTranslation } from 'react-i18next';
+import RadioOption from '../molecules/radioOption';
+import { LANGUAGE_SET } from '../../utils/language';
+
+export default function LanguageModal() {
+  const { t, i18n } = useTranslation();
+
+  const onLanguageSelect = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault();
+    const formData = new FormData(event.currentTarget);
+    const selectedLanguage = Object.fromEntries(formData.entries()).LANGUAGE;
+
+    if (selectedLanguage !== undefined
+      && i18n.language !== selectedLanguage.toString()) {
+      i18n.changeLanguage(selectedLanguage.toString());
+    }
+
+    window.location.hash = '';
+  };
+
+  return (
+    <>
+      <div role="dialog" aria-modal className="fixed bottom-0 w-full max-w-[500px] rounded-t-2xl bg-white">
+        <form onSubmit={onLanguageSelect}>
+          <section>
+            <h1 className="py-2 text-center">{t('landingPage.languageSelect')}</h1>
+            <ul className="h-[calc(50vh-6rem-1px)] overflow-y-auto overscroll-contain px-4">
+              {LANGUAGE_SET.map(({ name, value }) => (
+                <li key={value}>
+                  <RadioOption
+                    labelName={name}
+                    optionGroup="LANGUAGE"
+                    id={value}
+                    value={value}
+                    defaultChecked={value === i18n.language}
+                    autoFocus
+                  />
+                </li>
+              ))}
+            </ul>
+          </section>
+          <button
+            type="submit"
+            className="w-full border-t border-t-matgpt-gray/50 bg-white py-4"
+          >
+            {t('landingPage.save')}
+          </button>
+        </form>
+      </div>
+    </>
+  );
+}
--- src/components/modals/modalContainer.tsx
+++ src/components/modals/modalContainer.tsx
@@ -1,64 +1,30 @@
 import { createPortal } from 'react-dom';
-import { useEffect } from 'react';
-import { useModalSelector } from '../../hooks/store';
-import { useModal } from '../../hooks/modal';
 import ModalBackdrop from './modalBackdrop';
+import { useModalSelector } from '../../hooks/store';
+import LanguageModal from './languageModal';
 
 export default function ModalContainer({ children }: { children: React.ReactNode }) {
-  const modals = useModalSelector((state) => state.modal);
-  const { closeModal } = useModal();
+  const { type } = useModalSelector((state) => state.modal);
 
-  useEffect(() => {
-    const closeWhenBack = () => {
-      if (modals.length > 0) {
-        closeModal();
-      }
-    };
-    window.addEventListener('popstate', closeWhenBack);
-
-    return () => window.removeEventListener('popstate', closeWhenBack);
-  }, [modals]);
+  if (type === 'Language') {
+    return (
+      <>
+        <div aria-hidden>
+          {children}
+        </div>
+        {createPortal(
+          <ModalBackdrop>
+            <LanguageModal />
+          </ModalBackdrop>,
+          document.body,
+        )}
+      </>
+    );
+  }
 
   return (
     <>
       {children}
-      {modals.map(({ type }) => {
-        if (type === 'Language') {
-          return createPortal(
-            <ModalBackdrop>
-              <div>
-                Language
-                <button type="button" onClick={() => closeModal()}>close</button>
-              </div>
-            </ModalBackdrop>,
-            document.body,
-          );
-        }
-        if (type === 'Login') {
-          return createPortal(
-            <ModalBackdrop>
-              <div>
-                Login
-                <button type="button" onClick={() => closeModal()}>close</button>
-              </div>
-            </ModalBackdrop>,
-            document.body,
-          );
-        }
-        if (type === 'Search') {
-          return createPortal(
-            <ModalBackdrop>
-              <div>
-                Search
-                <button type="button" onClick={() => closeModal()}>close</button>
-              </div>
-            </ModalBackdrop>,
-            document.body,
-          );
-        }
-
-        return null;
-      })}
     </>
   );
 }
