116eaef980cd5f087a3fc000a07ab627f5d2412f
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-21 02:54:19 +0900

    feat: esc 입력 시 모달 닫기 적용

--- src/components/modals/modalContainer.tsx
+++ src/components/modals/modalContainer.tsx
@@ -31,6 +31,19 @@ export default function ModalContainer({ children }: { children: React.ReactNode
     };
   }, [type]);
 
+  useEffect(() => {
+    const handleEscape = (event: KeyboardEvent) => {
+      if (event.key === 'Escape') {
+        window.location.hash = '';
+      }
+    };
+    window.addEventListener('keydown', handleEscape);
+
+    return () => {
+      window.removeEventListener('keydown', handleEscape);
+    };
+  }, []);
+
   if (type === 'Language') {
     return (
       <>
