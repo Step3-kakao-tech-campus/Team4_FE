e90b6e25aa24191c458cc0c7e280259009d09642
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-21 17:22:03 +0900

    feat: 검색 모달 구현

--- src/components/modals/modalContainer.tsx
+++ src/components/modals/modalContainer.tsx
@@ -1,12 +1,15 @@
 import { createPortal } from 'react-dom';
 import { useEffect, useRef } from 'react';
+import { useLocation } from 'react-router-dom';
 import ModalBackdrop from './modalBackdrop';
 import { useModalSelector } from '../../hooks/store';
 import LanguageModal from './languageModal';
 import SocialLoginModalContent from './socialLoginModalContent';
+import SearchModal from './searchModal';
 
 export default function ModalContainer({ children }: { children: React.ReactNode }) {
   const { type } = useModalSelector((state) => state.modal);
+  const location = useLocation();
 
   const preNode = useRef<HTMLDivElement>(null);
   const postNode = useRef<HTMLDivElement>(null);
@@ -46,7 +49,7 @@ export default function ModalContainer({ children }: { children: React.ReactNode
     };
   }, []);
 
-  if (type === 'Language') {
+  if (type === 'Language' && location.hash === '#Language') {
     return (
       <>
         <div aria-hidden>
@@ -66,7 +69,7 @@ export default function ModalContainer({ children }: { children: React.ReactNode
     );
   }
 
-  if (type === 'Login') {
+  if (type === 'Login' && location.hash === '#Login') {
     return (
       <>
         <div aria-hidden>
@@ -77,6 +80,26 @@ export default function ModalContainer({ children }: { children: React.ReactNode
             <div role="dialog" aria-modal>
               <div ref={preNode} tabIndex={0} />
               <SocialLoginModalContent />
+              <div ref={postNode} tabIndex={0} />
+            </div>
+          </ModalBackdrop>,
+          document.body,
+        )}
+      </>
+    );
+  }
+
+  if (type === 'Search' && location.hash === '#Search') {
+    return (
+      <>
+        <div aria-hidden>
+          {children}
+        </div>
+        {createPortal(
+          <ModalBackdrop>
+            <div role="dialog" aria-modal>
+              <div ref={preNode} tabIndex={0} />
+              <SearchModal />
               <div ref={postNode} tabIndex={0} />
             </div>
           </ModalBackdrop>,
