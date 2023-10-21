274da974268d860d44031ef9694bcbcf9d476bde
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-21 15:21:53 +0900

    feat: 소셜 로그인 모달 커스텀 훅으로 migration

--- src/components/modals/modalContainer.tsx
+++ src/components/modals/modalContainer.tsx
@@ -3,6 +3,7 @@ import { useEffect, useRef } from 'react';
 import ModalBackdrop from './modalBackdrop';
 import { useModalSelector } from '../../hooks/store';
 import LanguageModal from './languageModal';
+import SocialLoginModalContent from './socialLoginModalContent';
 
 export default function ModalContainer({ children }: { children: React.ReactNode }) {
   const { type } = useModalSelector((state) => state.modal);
@@ -55,6 +56,26 @@ export default function ModalContainer({ children }: { children: React.ReactNode
             <div role="dialog" aria-modal>
               <div ref={preNode} tabIndex={0} />
               <LanguageModal />
+              <div ref={postNode} tabIndex={0} />
+            </div>
+          </ModalBackdrop>,
+          document.body,
+        )}
+      </>
+    );
+  }
+
+  if (type === 'Login') {
+    return (
+      <>
+        <div aria-hidden>
+          {children}
+        </div>
+        {createPortal(
+          <ModalBackdrop>
+            <div role="dialog" aria-modal>
+              <div ref={preNode} tabIndex={0} />
+              <SocialLoginModalContent />
               <div ref={postNode} tabIndex={0} />
             </div>
           </ModalBackdrop>,
