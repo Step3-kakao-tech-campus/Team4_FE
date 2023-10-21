4c4079ed59a8fbc949a39f46259ae078d7425c5a
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-21 20:40:24 +0900

    feat: 모달 배경 화면 전체에 적용되도록 수정

--- src/components/modals/languageModal.tsx
+++ src/components/modals/languageModal.tsx
@@ -20,31 +20,33 @@ export default function LanguageModal() {
 
   return (
     <>
-      <div className="fixed bottom-0 w-full max-w-[500px] rounded-t-2xl bg-white">
-        <form onSubmit={onLanguageSelect}>
-          <section>
-            <h1 className="py-2 text-center">{t('landingPage.languageSelect')}</h1>
-            <ul className="h-[calc(50vh-6rem-1px)] overflow-y-auto overscroll-contain px-4">
-              {LANGUAGE_SET.map(({ name, value }) => (
-                <li key={value}>
-                  <RadioOption
-                    labelName={name}
-                    optionGroup="LANGUAGE"
-                    id={value}
-                    value={value}
-                    defaultChecked={value === i18n.language}
-                  />
-                </li>
-              ))}
-            </ul>
-          </section>
-          <button
-            type="submit"
-            className="w-full border-t border-t-matgpt-gray/50 bg-white py-4"
-          >
-            {t('landingPage.save')}
-          </button>
-        </form>
+      <div className="h-[100dvh]">
+        <div className="fixed bottom-0 w-full max-w-[500px] rounded-t-2xl bg-white">
+          <form onSubmit={onLanguageSelect}>
+            <section>
+              <h1 className="py-2 text-center">{t('landingPage.languageSelect')}</h1>
+              <ul className="h-[calc(50vh-6rem-1px)] overflow-y-auto overscroll-contain px-4">
+                {LANGUAGE_SET.map(({ name, value }) => (
+                  <li key={value}>
+                    <RadioOption
+                      labelName={name}
+                      optionGroup="LANGUAGE"
+                      id={value}
+                      value={value}
+                      defaultChecked={value === i18n.language}
+                    />
+                  </li>
+                ))}
+              </ul>
+            </section>
+            <button
+              type="submit"
+              className="w-full border-t border-t-matgpt-gray/50 bg-white py-4"
+            >
+              {t('landingPage.save')}
+            </button>
+          </form>
+        </div>
       </div>
     </>
   );
--- src/components/modals/modalBackdrop.tsx
+++ src/components/modals/modalBackdrop.tsx
@@ -1,7 +1,9 @@
 export default function ModalBackdrop({ children }: { children: React.ReactNode }) {
   return (
-    <div className="fixed top-0 h-[100dvh] w-full max-w-[500px] bg-black/50">
-      {children}
+    <div className="fixed left-0 top-0 h-[100dvh] w-full bg-black/50">
+      <div className="absolute left-1/2 top-1/2 w-full max-w-[500px] -translate-x-1/2 -translate-y-1/2">
+        {children}
+      </div>
     </div>
   );
 }
