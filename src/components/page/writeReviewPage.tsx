58fd4a58a1f33f7c6f60c7759bcec6ba09aee368
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-23 17:33:19 +0900

    feat: 리뷰 내용 ref 연결

--- src/components/page/writeReviewPage.tsx
+++ src/components/page/writeReviewPage.tsx
@@ -1,13 +1,16 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 import WriteReviewTemplate from '../template/writeReviewTemplate';
+import { WriteReviewRefHandler } from '../../types/refHandler';
 
 export default function WriteReviewPage() {
   const [rating, setRating] = useState(5);
+  const writeReviewRef = useRef<WriteReviewRefHandler>(null);
 
   return (
     <WriteReviewTemplate
       rating={rating}
       setRating={setRating}
+      ref={writeReviewRef}
     />
   );
 }
--- src/components/template/writeReviewTemplate.tsx
+++ src/components/template/writeReviewTemplate.tsx
@@ -1,19 +1,35 @@
-import { Dispatch, SetStateAction } from 'react';
+import {
+  Dispatch, SetStateAction, forwardRef, useImperativeHandle, useRef,
+} from 'react';
 import Button from '../atoms/button';
 import Input from '../atoms/input';
 import PageTitleCard from '../molecules/pageTitleCard';
 import SelectRating from '../molecules/selectRating';
 import UploadImage from '../organisms/uploadImage';
+import { RefHandler, WriteReviewRefHandler } from '../../types/refHandler';
 
 interface WriteReviewTemplateProps {
   rating: number;
   setRating: Dispatch<SetStateAction<number>>;
 }
 
-export default function WriteReviewTemplate({
-  rating,
-  setRating,
-}: WriteReviewTemplateProps) {
+const WriteReviewTemplate = forwardRef<WriteReviewRefHandler, WriteReviewTemplateProps>((
+  {
+    rating,
+    setRating,
+  },
+  ref,
+) => {
+  const contentRef = useRef<RefHandler>(null);
+  const peopleCountRef = useRef<RefHandler>(null);
+  const totalPriceRef = useRef<RefHandler>(null);
+
+  useImperativeHandle(ref, () => ({
+    getContent: () => contentRef.current?.getInputValue(),
+    getPeopleCount: () => peopleCountRef.current?.getInputValue(),
+    getTotalPrice: () => totalPriceRef.current?.getInputValue(),
+  }));
+
   return (
     <main className="pb-[3.7rem]">
       <PageTitleCard pageTitle="리뷰 작성" />
@@ -27,19 +43,28 @@ export default function WriteReviewTemplate({
           가게에 대한 리뷰를 남겨주세요.
         </div>
         <div className="h-80">
-          <Input mode="multiLine" />
+          <Input
+            mode="multiLine"
+            ref={contentRef}
+          />
         </div>
         <div className="flex flex-col items-center">
           <div>몇 분이서 오셨나요?</div>
           <div className="flex w-24 items-center gap-2">
-            <Input mode="singleLine" />
+            <Input
+              mode="singleLine"
+              ref={peopleCountRef}
+            />
             명
           </div>
         </div>
         <div className="flex flex-col items-center">
           <div>얼마나 지불하셨나요?</div>
           <div className="flex w-36 items-center gap-2">
-            <Input mode="singleLine" />
+            <Input
+              mode="singleLine"
+              ref={totalPriceRef}
+            />
             원
           </div>
         </div>
@@ -50,4 +75,6 @@ export default function WriteReviewTemplate({
       </div>
     </main>
   );
-}
+});
+
+export default WriteReviewTemplate;
