fe6e6f870c6e5a1001e30b33a5725f3b42ec1bfe
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-25 21:15:56 +0900

    feat: 메뉴 태그 redux로 상태 관리

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { modalReducer } from './slices/modalSlice';
+import { menuTagReducer } from './slices/menuTagSlice';
 
 export const store = configureStore({
   reducer: {
     modal: modalReducer,
+    menuTag: menuTagReducer,
   },
 });
 
---
+++ src/store/slices/menuTagSlice.ts
@@ -0,0 +1,35 @@
+import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import { ReviewImageTagInfo } from '../../types/review';
+
+const initialState: ReviewImageTagInfo[] = [];
+
+export const menuTagSlice = createSlice({
+  name: 'menuTag',
+  initialState,
+  reducers: {
+    addMenuTag: (state, action: PayloadAction<{
+      imageIndex: number,
+      locationX: number,
+      locationY: number
+    }>) => {
+      state.push({
+        tagIndex: state[state.length - 1].tagIndex + 1,
+        name: '',
+        imageIndex: action.payload.imageIndex,
+        locationX: action.payload.locationX,
+        locationY: action.payload.locationY,
+      });
+    },
+    removeMenuTag: (
+      state,
+      action: PayloadAction<{ tagIndex: number }>,
+    ) => state.filter((tag) => tag.tagIndex !== action.payload.tagIndex),
+    removeAllMenuTagFromCurrentImage: (
+      state,
+      action: PayloadAction<{ imageIndex: number }>,
+    ) => state.filter((tag) => tag.imageIndex !== action.payload.imageIndex),
+  },
+});
+
+export const { addMenuTag, removeMenuTag, removeAllMenuTagFromCurrentImage } = menuTagSlice.actions;
+export const menuTagReducer = menuTagSlice.reducer;
