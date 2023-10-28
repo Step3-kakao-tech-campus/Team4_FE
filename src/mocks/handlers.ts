07b783bed5f0ec2afcbe3adbf204c236788784d8
gomdarachi <=>
2023-10-28 22:11:12 +0900

    refactor: msw데이터 모듈화

--- src/mocks/handlers.ts
+++ src/mocks/handlers.ts
@@ -2,6 +2,7 @@ import { rest } from 'msw';
 import { writedReviewData } from './data/writedReview';
 import recentlyViewdStoreData from './data/recentlyViewdStore';
 import { coinRechargeData, coinUsageData } from './data/coinData';
+import { reviewData } from './data/reviewData';
 
 export const handlers = [
   rest.get('/search', (req, res, ctx) => {
@@ -364,45 +365,7 @@ export const handlers = [
 
     return res(
       ctx.status(200),
-      ctx.json({
-        success: true,
-        response: {
-          content: 'Lorem ipsum dolor sit amet consectetur. Tellus erat arcu mauris maecenas condimentum tellus laoreet sagittis eleifend. Auctor massa praesent non ac et vitae. Nisl enim est faucibus nunc sociis faucibus. Tellus risus iaculis id in id. Eget bibendum dictum morbi sit diam rutrum at. Risus condimentum curabitur arcu aliquam lorem. Purus feugiat ac id scelerisque. Vel eget imperdiet leo at arcu platea vivamus gravida leo.',
-          peopleCount: 2,
-          rating: 5,
-          reviewImages: [{
-            imageData: '/image/fakeDb/review/reviewImage.png',
-            tags: [{
-              imageIndex: 0,
-              tagindex: 0,
-              name: '짜장면',
-              locationX: 61.8,
-              locationY: 26.8,
-              rating: 5,
-            },
-            {
-              imageIndex: 0,
-              tagindex: 1,
-              name: '탕수육',
-              locationX: 80.8,
-              locationY: 6.8,
-              rating: 1,
-            }],
-          }, {
-            imageData: '/image/fakeDb/store/store2.png',
-            tags: [{
-              imageIndex: 1,
-              tagindex: 0,
-              name: '삼겹살',
-              locationX: 40.8,
-              locationY: 40.8,
-              rating: 4,
-            }],
-          }],
-          totalPrice: 33000,
-        },
-        error: null,
-      }),
+      ctx.json(reviewData),
     );
   }),
 ];
