93360438c08ba9e1acccea5bc1f880a4d93dc816
gomdarachi <=>
2023-10-29 12:25:18 +0900

    feat: 프롬프트에 메뉴 추가하는 clickHandle함수 구현

--- src/components/organisms/reviewImageCarousel.tsx
+++ src/components/organisms/reviewImageCarousel.tsx
@@ -7,20 +7,26 @@ import { useModal } from '../../hooks/modal';
 
 interface ReviewImageCarouselProps {
   reviewImages: ReviewImageInfo[],
+  prompts: { [key: string]: number },
   setPrompts: React.Dispatch<React.SetStateAction<{}>>;
-
 }
 
-function ReviewImageCarousel({ reviewImages, setPrompts }: ReviewImageCarouselProps) {
+function ReviewImageCarousel({ reviewImages, prompts, setPrompts }: ReviewImageCarouselProps) {
   const { openModal } = useModal('Login');
 
-  const onHandlePromptEvent = () => {
+  const onHandlePromptEvent = (name: string) => {
     const token = localStorage.getItem('token');
     console.log(token);
-    if (token === null) {
+    if (token !== null) {
       openModal();
-    } else {
-      setPrompts('dsd');
+    } else if (prompts[name] === undefined) {
+      // 프롬프트에 메뉴가 없다면
+      setPrompts((prev) => ({
+        ...prev,
+        [name]: 1,
+      }));
+    } else { // 프롬프트에 메뉴가 있다면
+      alert('이미 추가된 메뉴 입니다');
     }
   };
 
@@ -38,7 +44,15 @@ function ReviewImageCarousel({ reviewImages, setPrompts }: ReviewImageCarouselPr
               tagIndex, locationX, locationY, name, rating,
             }) => (
               <li key={tagIndex} className="absolute" style={{ top: `${locationY}%`, left: `${locationX}%` }}>
-                <MenuTag tagIndex={tagIndex} name={name} rating={rating} mode="prompt" onPromptEvent={() => { onHandlePromptEvent(); }} />
+                <MenuTag
+                  tagIndex={tagIndex}
+                  name={name}
+                  rating={rating}
+                  mode="prompt"
+                  onPromptEvent={
+                    () => { onHandlePromptEvent(name); }
+                  }
+                />
               </li>
             ))}
           </div>
--- src/components/template/reviewDetailTemplate.tsx
+++ src/components/template/reviewDetailTemplate.tsx
@@ -17,7 +17,11 @@ function ReviewDetailTemplate({ data }: ReviewDetailTemplateProps) {
   return (
     <main>
       <PageTitleCard pageTitle="리뷰 상세 페이지" />
-      <ReviewImageCarousel reviewImages={data.reviewImages} setPrompts={setPrompts} />
+      <ReviewImageCarousel
+        reviewImages={data.reviewImages}
+        prompts={prompts}
+        setPrompts={setPrompts}
+      />
       <ReviewInformation
         rating={data.rating}
         createdAt={data.createdAt}
@@ -27,16 +31,18 @@ function ReviewDetailTemplate({ data }: ReviewDetailTemplateProps) {
         totalPrice={data.totalPrice}
       />
       {isClick ? <PromptEdit prompts={prompts} /> : (
-        <section className="mb-20 px-2">
-          {data.content}
-        </section>
+        <div>
+          <section className="mb-20 px-2">
+            {data.content}
+          </section>
+          <div className="solid fixed bottom-16 right-4 z-10 mb-10 h-5 w-5 rounded-3xl bg-matgpt-red text-center leading-4">
+            <span className="text-[0.75rem] font-bold">12</span>
+          </div>
+          <button type="button" onClick={() => { setIsClick((prev) => !prev); }} className="solid fixed bottom-8 right-5 mb-10 rounded-3xl border-8 border-matgpt-blue bg-matgpt-blue">
+            <Icon name="JournalBookmarkFill" ariaLabel="주문 프롬프트 수정 페이지 열기" size="1.5rem" />
+          </button>
+        </div>
       )}
-      <div className="solid fixed bottom-16 right-4 z-10 mb-10 h-5 w-5 rounded-3xl bg-matgpt-red text-center leading-4">
-        <span className="text-[0.75rem] font-bold">12</span>
-      </div>
-      <button type="button" onClick={() => { setIsClick((prev) => !prev); }} className="solid fixed bottom-8 right-5 mb-10 rounded-3xl border-8 border-matgpt-blue bg-matgpt-blue">
-        <Icon name="JournalBookmarkFill" ariaLabel="주문 프롬프트 수정 페이지 열기" size="1.5rem" />
-      </button>
     </main>
   );
 }
