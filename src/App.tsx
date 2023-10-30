5a441d72ba91dbaf03944aec1be6e7c5b8f72ef1
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-30 21:21:00 +0900

    feat: Error boundary 구현

--- src/App.tsx
+++ src/App.tsx
@@ -20,6 +20,7 @@ import UserEditProfilePage from './components/page/userEditProfilePage';
 import CoinUsagePage from './components/page/coinUsagePage';
 import LikedReviewPage from './components/page/likedReviewPage';
 import WriteReviewPage from './components/page/writeReviewPage';
+import ErrorBoundary from './components/layouts/errorBoundary';
 
 if (process.env.NODE_ENV === 'development') {
   worker.start({
@@ -35,25 +36,27 @@ function App() {
       <Provider store={store}>
         <BrowserRouter>
           <ModalContainer>
-            <Routes>
-              {/* 공통 레이아웃 */}
-              <Route element={<MainLayout />}>
-                <Route path="/" element={<MainPage />} />
-                <Route path="/mypage" element={<UserPage />} />
-                <Route path="/search" element={<SearchResultPage />} />
-                <Route path="/stores/:storeId" element={<StoreDetailPage />} />
-                <Route path="/likedStores" element={<LikedStorePage />} />
-                <Route path="/coinRechargeHistory" element={<CoinRechargePage />} />
-                <Route path="/writtenReview" element={<WritedReviewPage />} />
-                <Route path="/likedReview" element={<LikedReviewPage />} />
-                <Route path="/recentStores" element={<RecentlyViewdStorePage />} />
-                <Route path="/coinUsageHistory" element={<CoinUsagePage />} />
-                <Route path="/stores/:storeId/writeReview" element={<WriteReviewPage />} />
-                <Route path="/profileEditing" element={<UserEditProfilePage />} />
-              </Route>
-              {/* 단독 레이아웃 */}
-              <Route path="/landing" element={<LandingPage />} />
-            </Routes>
+            <ErrorBoundary>
+              <Routes>
+                {/* 공통 레이아웃 */}
+                <Route element={<MainLayout />}>
+                  <Route path="/" element={<MainPage />} />
+                  <Route path="/mypage" element={<UserPage />} />
+                  <Route path="/search" element={<SearchResultPage />} />
+                  <Route path="/stores/:storeId" element={<StoreDetailPage />} />
+                  <Route path="/likedStores" element={<LikedStorePage />} />
+                  <Route path="/coinRechargeHistory" element={<CoinRechargePage />} />
+                  <Route path="/writtenReview" element={<WritedReviewPage />} />
+                  <Route path="/likedReview" element={<LikedReviewPage />} />
+                  <Route path="/recentStores" element={<RecentlyViewdStorePage />} />
+                  <Route path="/coinUsageHistory" element={<CoinUsagePage />} />
+                  <Route path="/stores/:storeId/writeReview" element={<WriteReviewPage />} />
+                  <Route path="/profileEditing" element={<UserEditProfilePage />} />
+                </Route>
+                {/* 단독 레이아웃 */}
+                <Route path="/landing" element={<LandingPage />} />
+              </Routes>
+            </ErrorBoundary>
           </ModalContainer>
         </BrowserRouter>
       </Provider>
--- src/utils/query.ts
+++ src/utils/query.ts
@@ -3,6 +3,7 @@ import { QueryClient } from 'react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      useErrorBoundary: true,
       refetchOnWindowFocus: false,
     },
   },
