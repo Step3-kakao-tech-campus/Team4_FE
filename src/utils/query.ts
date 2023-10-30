2b434691b0b15b30ec7c542d3b76f9be6d450a47
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-30 21:22:07 +0900

    feat: react-query suspense 옵션 추가

--- src/utils/query.ts
+++ src/utils/query.ts
@@ -3,6 +3,7 @@ import { QueryClient } from 'react-query';
 export const queryClient = new QueryClient({
   defaultOptions: {
     queries: {
+      suspense: true,
       useErrorBoundary: true,
       refetchOnWindowFocus: false,
     },
