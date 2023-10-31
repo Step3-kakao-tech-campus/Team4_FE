1e8153c7d5e63cbcd0aa418ea670f3d3931cf4f0
Donghyeon Lee <ehdgus001928@gmail.com>
2023-10-31 21:07:00 +0900

    feat: 리뷰 삭제 모달 복구
    - 리뷰 상세 페이지에 종속적이므로 useModal hook에는 적합하지 않다고
      판단됨

--- src/components/modals/deleteReviewModal.tsx
+++ src/components/modals/deleteReviewModal.tsx
@@ -1,44 +1,64 @@
+import React, { useState } from 'react';
 import { useTranslation } from 'react-i18next';
+import ReactModal from 'react-modal';
 import Button from '../atoms/button';
+
+const customModalStyles = {
+  overlay: {
+    backgroundColor: 'rgba(0, 0, 0, 0.4)',
+    width: '100%',
+    zIndex: '10',
+    top: '0',
+    left: '0',
+  },
+  content: {
+    width: '300px',
+    height: '12rem',
+    zIndex: '150',
+    top: '50%',
+    left: '50%',
+    transform: 'translate(-50%, -50%)',
+    borderRadius: '10px',
+    boxShadow: '2px 2px 2px rgba(0, 0, 0, 0.25)',
+    backgroundColor: 'white',
+    overflow: 'auto',
+  },
+};
 
 function DeleteReviewModal() {
   const { t } = useTranslation();
+  const [modalOpen, setModalOpen] = useState(true);
 
-  function handleDeleteReview() {
-
+  function onCloseModalClick() {
+    setModalOpen(false);
   }
 
   return (
-    <div className="flex h-48 w-[300px] flex-col
-      justify-center justify-items-center rounded-[10px] bg-white"
+    <ReactModal
+      isOpen={modalOpen}
+      style={customModalStyles}
     >
-      <div className="text-center">{t('deleteReviewModal.deleteTitle')}</div>
-      <div className="text-center font-bold text-matgpt-red">{t('deleteReviewModal.notRecovredTitle')}</div>
-      <div className="mt-12 text-center">
-        <Button
-          size="medium"
-          backgroundColor="bg-matgpt-gray"
-          textColor="text-black"
-          extraStyle="mr-8"
-          onClick={() => {
-            window.location.hash = '';
-          }}
-        >
-          {t('deleteReviewModal.cancel')}
-        </Button>
-        <Button
-          size="medium"
-          backgroundColor="bg-matgpt-red"
-          textColor="text-black"
-          onClick={() => {
-            handleDeleteReview();
-            //  백앤드로 리뷰 삭제 요청을 보낸다.
-          }}
-        >
-          삭제
-        </Button>
+      <div className="w-full flex-col justify-center justify-items-center">
+        <div className="text-center">{t('deleteReviewModal.deleteTitle')}</div>
+        <div className="text-center font-bold text-matgpt-red">{t('deleteReviewModal.notRecovredTitle')}</div>
+        <div className="mt-12 text-center">
+          <Button size="medium" backgroundColor="bg-matgpt-gray" textColor="text-black" extraStyle="mr-8" onClick={() => { onCloseModalClick(); }}>
+            {t('deleteReviewModal.cancel')}
+          </Button>
+          <Button
+            size="medium"
+            backgroundColor="bg-matgpt-red"
+            textColor="text-black"
+            onClick={() => {
+              onCloseModalClick();
+              //  백앤드로 리뷰 삭제 요청을 보낸다.
+            }}
+          >
+            삭제
+          </Button>
+        </div>
       </div>
-    </div>
+    </ReactModal>
   );
 }
 
