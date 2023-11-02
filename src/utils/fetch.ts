ee3e20b7213e8a97d18709895dbfb5624df0029c
gomdarachi <=>
2023-11-03 00:28:14 +0900

    feat: 로그인 시 헤더에 AccessToken 넣기

--- src/utils/fetch.ts
+++ src/utils/fetch.ts
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import moment from 'moment';
 
 export const createFetchInstance = (
   baseURL: string | undefined,
@@ -10,6 +11,31 @@ export const createFetchInstance = (
     headers: {
       'Content-Type': 'application/json',
     },
+  });
+
+  instance.interceptors.request.use((config) => {
+    const newConfig = { ...config };
+    const accessToken = localStorage.getItem('accessToken');
+    if (accessToken !== 'null') {
+      const accessTokenExpiresIn = localStorage.get('accessTokenExpiresIn');
+      if (moment(accessTokenExpiresIn).diff(moment()) < 0) { // 발급 유효 시간 초과 시 재 발급
+        axios({
+          url: 'api url',
+          method: 'post',
+          headers: {
+            accessToken: localStorage.getItem('accessToken'),
+            refreshToken: localStorage.getItem('refreshToken'),
+          },
+        }).then((res) => {
+          localStorage.setItem('accessToken', `Bearer ${res.data.accessToken}`);
+          localStorage.setItem('accessTokenExpiresIn', `Bearer ${res.data.accessTokenExpiresIn}`);
+        });
+      }
+      newConfig.headers.Authorization = accessToken;
+      return newConfig;
+    }
+
+    return config;
   });
 
   return instance;
