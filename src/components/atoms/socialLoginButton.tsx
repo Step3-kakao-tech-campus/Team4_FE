c8808287c78fed6ec5589fb0ca84ce13a1214045
gomdarachi <=>
2023-11-02 18:35:10 +0900

    feat: 소샬 로그인 모달창 열 때의  url 정보 전역 상태 값으로 저장

--- src/components/atoms/socialLoginButton.tsx
+++ src/components/atoms/socialLoginButton.tsx
@@ -1,25 +1,47 @@
 import { useTranslation } from 'react-i18next';
+import { useLocation, useNavigate } from 'react-router-dom';
+import { useDispatch } from 'react-redux/es/exports';
+import { editRedirectUrl } from '../../store/slices/redirectUrlSlice';
 
 interface SocialLoginButtonProps {
   name: 'kakao' | 'google';
 }
 
-function SocialLoginButton({ name } :SocialLoginButtonProps) {
+function SocialLoginButton({ name }: SocialLoginButtonProps) {
   const { t } = useTranslation();
+  const currentLocation = useLocation();
+  const navigate = useNavigate();
+  const dispatch = useDispatch();
+
+  const onClickHandleSocialLogin = (socialType: string) => {
+    dispatch(editRedirectUrl({
+      url: currentLocation.pathname,
+    }));
+    navigate(`http://localhost:8080/oauth2/authorization/${socialType}`);
+  };
+
   if (name === 'kakao') {
     return (
-      <a href="http://localhost:8080/oauth2/authorization/kakao" className="flex items-center justify-center bg-[#FEE500] w-[250px] h-[40px] rounded-xl mb-2">
+      <button
+        type="button"
+        onClick={() => { onClickHandleSocialLogin('kakao'); }}
+        className="mb-2 flex h-[40px] w-[250px] items-center justify-center rounded-xl bg-white shadow-md"
+      >
         <img src="/img/social-login/kakao-ballon.png" alt={t('loginModal.kakaoLogo')} className="w-6" />
-        <span className="ml-4 text-md">{t('loginModal.loginKakao')}</span>
-      </a>
+        <span className="text-md ml-4">{t('loginModal.loginKakao')}</span>
+      </button>
     );
   }
   if (name === 'google') {
     return (
-      <a href="http://localhost:8080/oauth2/authorization/Google" className="flex items-center justify-center bg-white w-[250px] h-[40px] shadow-md rounded-xl mb-2">
+      <button
+        type="button"
+        onClick={() => { onClickHandleSocialLogin('google'); }}
+        className="mb-2 flex h-[40px] w-[250px] items-center justify-center rounded-xl bg-white shadow-md"
+      >
         <img src="/img/social-login/google-logo.png" alt={t('loginModal.googleLogo')} className="w-6" />
-        <span className="ml-4 font-roboto text-md">{t('loginModal.loginGoogle')}</span>
-      </a>
+        <span className="text-md ml-4 font-roboto">{t('loginModal.loginGoogle')}</span>
+      </button>
     );
   }
   return (<div>해당하는 소셜 로그인 버튼이 없습니다.</div>);
