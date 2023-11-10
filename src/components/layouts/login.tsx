028ae301ddc9585ebca3cc674c298ad0bc2af6bd
gomdarachi <=>
2023-11-10 18:08:57 +0900

    feat: 비밀번호 보임/숨김 상태 구현

--- src/components/layouts/login.tsx
+++ src/components/layouts/login.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import PageTitleCard from '../molecules/pageTitleCard';
 import Button from '../atoms/button';
@@ -10,6 +10,9 @@ function Login() {
     // 로그인 상태인데 이 레이아웃에 들어오면 바로 메인 페이지로 이동
     if (localStorage.getItem('accessToken') !== null) { navigate('/'); }
   }, []);
+
+  const [isError] = useState(false);
+  const [isPasswordHide, setIsPasswordHide] = useState(true);
 
   return (
     <div className="flex flex-col items-center">
@@ -27,12 +30,34 @@ function Login() {
         <div className="mb-2">
           <input type="text" placeholder="아이디를 입력하세요." className="w-full rounded-full border border-black bg-white px-4 py-2" />
         </div>
-        <div>
-          <input type="password" placeholder="비밀번호를 입력하세요." className="w-full rounded-full border border-black bg-white px-4 py-2" />
+        <div className="relative">
+          <input type={isPasswordHide ? 'password' : 'text'} placeholder="비밀번호를 입력하세요." className="w-full rounded-full border border-black bg-white px-4 py-2" />
+          <div>
+            {isPasswordHide ? (
+              <button
+                className="absolute right-2 top-2"
+                type="button"
+                onClick={() => { setIsPasswordHide(false); }}
+              >
+                <Icon name="EyeCheck" ariaLabel="비밀번호 숨김 상태" size="1.3rem" />
+              </button>
+            )
+              : (
+                <button
+                  className="absolute right-2 top-3"
+                  type="button"
+                  onClick={() => { setIsPasswordHide(true); }}
+                >
+                  <Icon name="EyeClosed" ariaLabel="비밀번호 보임 상태" size="1.3rem" />
+                </button>
+              )}
+          </div>
         </div>
-        <div className="absolute bottom-[-2rem]">
-          <span>아이디가 틀렸습니다.</span>
-        </div>
+        {isError ? (
+          <div className="absolute bottom-[-2rem]">
+            <span>아이디가 틀렸습니다.</span>
+          </div>
+        ) : null}
       </div>
       <div className="mt-8">
         <Button size="large">로그인 하기</Button>
